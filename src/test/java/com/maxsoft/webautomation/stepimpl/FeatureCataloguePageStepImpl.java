6649513f7798512432c61697b4c1f3f028582adf
Kushani Karunarathne <kushani.karunarathne@cinglevue.com>
2018-12-20 15:01:05 +0530

    Create District

---
+++ src/test/java/com/maxsoft/webautomation/stepimpl/FeatureCataloguePageStepImpl.java
@@ -0,0 +1,26 @@
+package com.maxsoft.webautomation.stepimpl;
+
+import com.maxsoft.webautomation.pages.LoginPage;
+import com.maxsoft.webautomation.util.driver.Driver;
+import com.thoughtworks.gauge.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class FeatureCataloguePageStepImpl {
+
+    private WebDriver driver = Driver.driver;
+    private LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+
+    @Step("On login page")
+    public void navigateToLogin(){
+        loginPage.navigateToLogin();
+    }
+
+    @Step("Login to the application using the username as <username> and password as <password>")
+    public void login(String username, String password){
+        loginPage.login(username, password);
+    }
+
+
+}
